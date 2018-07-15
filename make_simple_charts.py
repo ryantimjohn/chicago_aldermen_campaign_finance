548d655b139c0e6c27484b72cae4f64d26fd3de9
nathanww <nathanww@gmail.com>
2018-07-15 12:36:40 -0500

    Create bar graphs showing how much of each alder's money comes from small donations and people in their ward. Graphs are output to the infographics folder and are named "<name> donation overview"

--- make_simple_charts.py
+++ make_simple_charts.py
@@ -8,6 +8,7 @@ from matplotlib import gridspec
 from matplotlib import rc
 from matplotlib import lines
 from matplotlib import font_manager as fm, rcParams
+import numpy as np
 import os
 
 all_aldermen='''Joe Moreno
@@ -61,6 +62,61 @@ Harry Osterman
 Joseph Moore
 Debra Silverstein'''.split("\n")
 
+def make_small_donor_chart(donation_data, alderman):
+    #Define the font parameters
+    matplotlib.rcParams['pdf.fonttype']= 42
+    matplotlib.rcParams['ps.fonttype']=42
+    plt.rc('text', usetex=True)
+    font = {'weight' : 'normal',
+            'size'   : 10}
+    matplotlib.rc('font', **font)
+    fpath = "Raleway-Black.ttf"
+    prop = fm.FontProperties(fname = fpath, weight='bold')
+    #set up the bar graph
+    ax=plt.gca()
+    bar_width = 1
+    opacity = 1
+    error_config = {'ecolor': '0.3'}
+    index=[0,1,2] #this just defines where things appear on X axis; we're not doing anything fancy o it's just a straight sequence ....
+    names=[' \\bf \Large \sffamily All funds','\\bf \Large \sffamily From small donors', '\\bf \Large \sffamily From people in their ward'] #X labels
+    colors=['#b51b89','#271ab5','#ff7b00'] #colors for each bar
+    r=index
+    rects = ax.bar(index, donation_data, bar_width,alpha=opacity, color=colors,label='Donations',align='center')
+    #display dollar amounts on top of bars
+    fmt = '\${x:,.0f}'
+    tick = ticker.StrMethodFormatter(fmt)
+    ax.yaxis.set_major_formatter(tick)
+
+    # X axis
+    plt.xticks(r, names)
+    
+    # X axis
+    plt.xticks(r, names)
+
+    # ticks
+    plt.tick_params(
+        axis='both',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom=False,      # ticks along the bottom edge are off
+        top=False,	# ticks along the top edge are off
+        left=False)
+
+    #spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_color('#989898')
+
+    #show the dollar totals on top of the bars
+    heights = np.array(donation_data)
+    labels=["\\bf \Large \sffamily \$ {:,.0f}".format(donation_data[0]),"\\bf \Large \sffamily \$ {:,.0f}".format(donation_data[1]),"\\bf \Large \sffamily \$ {:,.0f}".format(donation_data[2])] #format the total for each of the three bars
+    xs = [0,1,2]
+    for height, label, x in zip(heights, labels, xs):
+        ax.text(x, height+10, label, ha='center', va='bottom')
+    # Show graphic
+    plt.tight_layout()
+    plt.savefig(os.path.join('infographics', (alderman+' donation overview')), dpi=1000)
+    plt.close()
 def get_donation_data(alder_data): #parse the report file to get small donaitons, donations from inside ward, and total
     feo_itemized=float(alder_data[alder_data.find("FEO ITEMIZED:")+13:].split("\n")[0].replace("$","")) #parse out the total for itemized contributions that would still be under the FEO limit. Small donaitons=non itemized contributions + itemized contributions less than 175
     #non_itemized=float(alder_data[alder_data.find("SECTOR small donors:")+19:].split("\n")[0].replace("$",""))
@@ -87,7 +143,7 @@ for alderman in all_aldermen:
         alder_file=open(os.path.join("data","reports",alderman.replace(" ","_")+" report.txt"),'r')
         alder_file_lines=alder_file.read()
         alder_file.close()
-        print(str(get_donation_data(alder_file_lines)))
+        make_small_donor_chart((get_donation_data(alder_file_lines)),alderman)
     except FileNotFoundError:
         print("Couldn't load file")
         
