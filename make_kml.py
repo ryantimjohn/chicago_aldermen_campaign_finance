9d828227b582ae832ed77e9c35d1aea09954c022
ryantimjohn <ryantimjohn@gmail.com>
2018-09-02 18:22:12 -0500

    update kml encoding to utf8

--- make_kml.py
+++ make_kml.py
@@ -3,23 +3,24 @@ import os
 
 def make_kml():
     kml = ""
-    with open(os.path.join('kml', 'Boundaries - Wards (2015-).kml')) as f:
+    with open(os.path.join('kml', 'Boundaries - Wards (2015-).kml'),
+              encoding='utf8') as f:
         kml = f.read()
     soup = BeautifulSoup(kml, "lxml-xml")
     placemarks = soup.findAll('Placemark')
 
     start_template = '''<?xml version='1.0' encoding='UTF-8'?>
-    <kml xmlns:kml="http://earth.google.com/kml/2.2">
-      <Document id="featureCollection">
-    <Style id="defaultStyle">
-      <LineStyle>
-        <width>1.5</width>
-      </LineStyle>
-      <PolyStyle>
-        <color>7d8a30c4</color>
-      </PolyStyle>
-    </Style>
-      <Folder>'''
+<kml xmlns:kml="http://earth.google.com/kml/2.2">
+  <Document id="featureCollection">
+<Style id="defaultStyle">
+  <LineStyle>
+    <width>1.5</width>
+  </LineStyle>
+  <PolyStyle>
+    <color>7d8a30c4</color>
+  </PolyStyle>
+</Style>
+  <Folder>'''
 
     end_template='''</Folder>  </Document>
     </kml>'''
@@ -31,7 +32,8 @@ def make_kml():
         placemark.find('ExtendedData').replaceWith(name.find('name'))
         placemark = str(placemark)
 
-        with open(os.path.join('kml', '{}.kml'.format(ward)), 'w') as f:
+        with open(os.path.join('kml', '{}.kml'.format(ward)), 'w',
+                  encoding='utf8') as f:
             f.write(start_template + placemark + end_template)
 
 make_kml()
